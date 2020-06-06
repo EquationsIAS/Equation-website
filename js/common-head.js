64b7c987f67846d6b8d1512160c1aafd5fff02e1
Devashish Mamgain <devashish@Devashishs-MacBook-Air.local>
2020-06-06 23:01:26 +0530

    add google tag manager

---
+++ js/common-head.js
@@ -0,0 +1,14 @@
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WL6ZRQH');
+
+
+(function(d, m){
+    var kommunicateSettings = {"appId":"35a308422564e4cf1cab3613837582837","popupWidget":true,"automaticChatOpenOnNavigation":true};
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    window.kommunicate = m; m._globals = kommunicateSettings;
+  })(document, window.kommunicate || {});
